ce4f864e23912742873f132d35ae5ce8add6d4c3
Ruby Wu <rubywu@rubydembp.wifi.local.cmu.edu>
2024-01-16 17:57:35 -0500

    first commit

---
+++ src/components/MessageBox.tsx
@@ -0,0 +1,38 @@
+import ReactMarkdown from 'react-markdown';
+import { useColorModeValue } from '@chakra-ui/react';
+import Card from '@/components/card/Card';
+
+export default function MessageBox(props: {
+  output: string;
+  images: string[];
+}) {
+  const { output, images } = props;
+  const textColor = useColorModeValue('navy.700', 'white');
+  return (
+    <Card
+      display={output ? 'flex' : 'none'}
+      px="22px !important"
+      pl="22px !important"
+      color={textColor}
+      minH="450px"
+      fontSize={{ base: 'sm', md: 'md' }}
+      lineHeight={{ base: '24px', md: '26px' }}
+      fontWeight="500"
+    >
+      <ReactMarkdown className="font-medium">
+        {output ? output : ''}
+      </ReactMarkdown>
+      {images && (
+        <div>
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={`data:image/jpeg;base64,${image}`}
+              alt={`Response Image ${index}`}
+            />
+          ))}
+        </div>
+      )}
+    </Card>
+  );
+}
